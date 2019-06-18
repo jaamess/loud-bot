4c0ad21506b78f2efa20d4d52edc4f1239258c68
James <thiagozevedo3@gmail.com>
2019-06-18 04:44:19 -0300

    messageReactionAdd custom event

---
+++ events/messageReactionAdd.js
@@ -0,0 +1,20 @@
+const { Event } = require('klasa');
+
+module.exports = class extends Event {
+  constructor(...args) {
+    super(...args, {
+      name: 'messageReactionAdd',
+      enabled: true,
+      event: 'messageReactionAdd',
+      once: false,
+    });
+  }
+
+  run(reaction, user) {
+    if (reaction.emoji.name === '❌') {
+      if (user.roles.position >= 23) return;
+      for (const users of reaction.users) reaction.users.remove(user.id);
+      return console.log(`Removed reaction ${reaction.emoji.name} from user ${user.username}`);
+    }
+  }
+};

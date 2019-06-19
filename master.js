964570945bc1be685e9602e253b4f786ad5d85cf
James <thiagozevedo3@gmail.com>
2019-06-19 20:16:21 -0300

    fix permission level 5

--- master.js
+++ master.js
@@ -2,7 +2,7 @@ const { Client } = require('klasa');
 const { prefix, token } = require('./ecosystem.config.json');
 
 Client.defaultPermissionLevels.add(5, (message) => {
-  message.member.roles.highest.position >= 23;
+  message.member.roles.highest.position >= 20;
 });
 
 new Client({

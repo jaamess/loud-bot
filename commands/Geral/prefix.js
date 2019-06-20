33051bf06759e515dcbd71ccdc27fbe7fd0dd00e
James <thiagozevedo3@gmail.com>
2019-06-19 21:53:45 -0300

    fix bug

--- commands/Geral/prefix.js
+++ commands/Geral/prefix.js
@@ -21,7 +21,7 @@ module.exports = class extends Command {
     if (oldPrefix === newPrefix[0]) return message.send('O prefixo da guilda já é este, não há nada pra mudar.');
     if (!newPrefix[0]) {
       message.send(message.language.get('COMMAND_PREFIX_RESET'));
-      message.guild.settings.reset('prefix');
+      return message.guild.settings.reset('prefix');
     }
 
     message.guild.settings.update('prefix', newPrefix[0]);

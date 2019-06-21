e1188dcb3e9a0a285e6b39aab4c5380e7b46fa71
James <thiagozevedo3@gmail.com>
2019-06-21 03:00:01 -0300

    add new permission level

--- master.js
+++ master.js
@@ -2,8 +2,9 @@ const { Client } = require('klasa');
 const { prefix, token } = require('./ecosystem.config.json');
 
 Client.defaultPermissionLevels
-  .add(5, (message) => message.member.roles.highest.position >= 20)
-  .add(6, (message) => message.member.permissions.has('ADMINISTRATOR'));
+  .add(4, (message) => message.member.roles.has('585898221415563275')) // Torcedor Hyper
+  .add(5, (message) => message.member.roles.highest.position >= 20) // Ajudante
+  .add(6, (message) => message.member.permissions.has('ADMINISTRATOR')); // Admin
 
 new Client({
   fetchAllMembers: true,

9a6fb8e1a5d87bbec2b65ba87cb06e264f1949b9
James <thiagozevedo3@gmail.com>
2019-06-21 04:26:10 -0300

    update emojis

--- monitors/rankVerification.js
+++ monitors/rankVerification.js
@@ -31,7 +31,7 @@ module.exports = class extends Monitor {
     if (!screenshot.length) {
       if (message.member.roles.highest.position < 28) {
         message.delete();
-        const response = await message.send(':warning:  |  **Você não enviou uma captura de tela válida para verificar sua patente. Tente novamente.**');
+        const response = await message.send(':warning~1:  |  **Você não enviou uma captura de tela válida para verificar sua patente. Tente novamente.**');
         setTimeout(() => {
           response.delete();
         }, 5000);
@@ -56,7 +56,7 @@ module.exports = class extends Monitor {
     if (!score) {
       working.delete();
       const errorMessage = await message.channel.send(
-        `:warning:  **|  Não foi possível verificar sua patente. Verifique se a captura de tela está correta e/ou tente novamente com uma imagem de maior qualidade.**`
+        `:warning~1:  **|  Não foi possível verificar sua patente. Verifique se a captura de tela está correta e/ou tente novamente com uma imagem de maior qualidade.**`
       );
       setTimeout(() => {
         errorMessage.delete();
@@ -73,20 +73,21 @@ module.exports = class extends Monitor {
     // Note that these emojis only exist in the server this bot was made for,
     // you should change the IDs in case you use this in a different server.
     const emoji = {
-      guild: this.client.guilds.get('572691736707596288'),
-      mestre: '591340535164436480',
-      diamante: '591340533235056640',
-      platina: '591498756780720148',
-      ouro: '591340532911833105',
-      prata: '591340534379970590',
-      bronze: '591340532882472961',
+      guild: this.client.guilds.get('550143369184280607'),
+      mestre: '591518007105290250',
+      diamante: '591518018257813504',
+      platina: '591518019315040275',
+      ouro: '591518006111109120',
+      prata: '591518006056714250',
+      bronze: '591518014831067138',
     };
     if (rank.startsWith('Mestre')) message.react(emoji.guild.emojis.get(emoji.mestre));
-    if (rank.startsWith('Diamante')) message.react(emoji.guild.emojis.get(emoji.diamante));
-    if (rank.startsWith('Platina')) message.react(emoji.guild.emojis.get(emoji.platina));
-    if (rank.startsWith('Ouro')) message.react(emoji.guild.emojis.get(emoji.ouro));
-    if (rank.startsWith('Prata')) message.react(emoji.guild.emojis.get(emoji.prata));
-    if (rank.startsWith('Bronze')) message.react(emoji.guild.emojis.get(emoji.bronze));
+    else if (rank.startsWith('Diamante')) message.react(emoji.guild.emojis.get(emoji.diamante));
+    else if (rank.startsWith('Platina')) message.react(emoji.guild.emojis.get(emoji.platina));
+    else if (rank.startsWith('Ouro')) message.react(emoji.guild.emojis.get(emoji.ouro));
+    else if (rank.startsWith('Prata')) message.react(emoji.guild.emojis.get(emoji.prata));
+    else if (rank.startsWith('Bronze')) message.react(emoji.guild.emojis.get(emoji.bronze));
+    // Delete the message that says we're processing the image
     working.delete();
     // Finished reacting to the image
     // Gives the user their role
@@ -137,7 +138,7 @@ module.exports = class extends Monitor {
      * @param {import('klasa').KlasaMessage} message
      */
     function addRole(message, rank) {
-      if (message.guild.me.roles.highest.position < message.guild.roles.get(rank).position) return message.send(`:warning:  **|  O meu cargo não é alto o suficiente para realizar esta ação.**`);
+      if (message.guild.me.roles.highest.position < message.guild.roles.get(rank).position) return message.send(`:warning~1:  **|  O meu cargo não é alto o suficiente para realizar esta ação.**`);
       return message.member.roles.add(rank);
     }
   }

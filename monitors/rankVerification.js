58c50ef0674a86ec199de9e78d0cff8b61f6b7d5
James <thiagozevedo3@gmail.com>
2019-06-20 21:22:32 -0300

    beginning development of tesseract OCR

--- monitors/rankVerification.js
+++ monitors/rankVerification.js
@@ -1,4 +1,7 @@
 const { Monitor } = require('klasa');
+const Tesseract = require('tesseract.js');
+const { TesseractWorker, OEM } = Tesseract;
+const worker = new TesseractWorker();
 
 module.exports = class extends Monitor {
   constructor(...args) {
@@ -14,31 +17,40 @@ module.exports = class extends Monitor {
       ignoreBlacklistedGuilds: true,
     });
   }
+  /**
+   * @param {import('klasa').KlasaMessage} message
+   */
 
-  run(message) {
-    /* This code is commented out because it is incomplete and will not be used for now
-     * Do not remove the comments
-     * unless you know exactly what you are doing
+  async run(message) {
+    /* This code is incomplete and will not be used for now
+     * First I'm getting the image url if it exists and feeding it to the OCR software
+     * Then I need to filter specific numbers to see what rank the user is
      */
-    // if (message.channel.id !== '') return;
-    // this.react(message);
+    if (message.channel.id !== '591340583159595028') return;
+    const screenshot = message.attachments.map((m) => m.attachment);
+    if (!screenshot.length) {
+      const response = await message.send(':warning:  |  **Você não enviou uma captura de tela válida para verificar sua patente. Tente novamente.**');
+      setTimeout(() => {
+        response.delete();
+      }, 5000);
+      return message.delete(1000);
+    }
+    return this.parseImage(screenshot);
   }
 
-  react(message) {
-    setTimeout(() => {
-      message.react(message.guild.emojis.get(''));
-    }, 850);
-    setTimeout(() => {
-      message.react(message.guild.emojis.get(''));
-    }, 850);
-    setTimeout(() => {
-      message.react(message.guild.emojis.get(''));
-    }, 850);
-    setTimeout(() => {
-      message.react(message.guild.emojis.get(''));
-    }, 850);
-    setTimeout(() => {
-      message.react(message.guild.emojis.get(''));
-    }, 850);
+  parseImage(image) {
+    worker
+      .recognize('https://tesseract.projectnaptha.com/img/eng_bw.png', 'eng', {
+        tessedit_ocr_engine_mode: OEM.TESSERACT_ONLY,
+        tessedit_char_whitelist: '0123456789-.',
+      })
+      .progress((p) => {
+        console.log('progress', p);
+      })
+      .then(({ text }) => {
+        console.log(text);
+        worker.terminate();
+      });
+    return;
   }
 };

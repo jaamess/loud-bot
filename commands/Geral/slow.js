b673465d98b3059b2c06904f907137e08419602a
James <thiagozevedo3@gmail.com>
2019-06-22 19:50:26 -0300

    add duration parser

--- commands/Geral/slow.js
+++ commands/Geral/slow.js
@@ -1,5 +1,6 @@
 const { Command } = require('klasa');
 const { MessageEmbed } = require('discord.js');
+const parseTime = require('duration-parser');
 
 module.exports = class extends Command {
   constructor(...args) {
@@ -8,7 +9,7 @@ module.exports = class extends Command {
       enabled: true,
       runIn: ['text'],
       permissionLevel: 6,
-      usage: '<Canal:channel> <Tempo:int>',
+      usage: '<Canal:channel> <Tempo:string>',
       usageDelim: ' ',
       description: 'Ativa o modo lento no canal especificado. Permite que o administrador ative o modo slow por quanto tempo quiser, sem depender dos limites do Discord.',
       extendedHelp: 'Para desativar o modo lento, defina a duração como "0" segundos.',
@@ -16,6 +17,7 @@ module.exports = class extends Command {
   }
 
   async run(message, [channel, duration]) {
+    parseTime(duration);
     channel.setRateLimitPerUser(duration, 'Modo slow customizado');
 
     const embed = new MessageEmbed()

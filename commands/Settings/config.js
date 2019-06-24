6cd8a1d3fb2770fb34f938d52e1ba6df212d6742
James <thiagozevedo3@gmail.com>
2019-06-24 05:31:33 -0300

    reorganising directories and config command

---
+++ commands/Settings/config.js
@@ -0,0 +1,41 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      permissionLevel: 5,
+      description: 'Mostra as configurações atuais no servidor',
+      extendedHelp: 'config reacoes -> Mostra as configurações atuais para a função de reações customizadas, como canais permitidos e palavras-chave.',
+      usage: '[reacoes]',
+    });
+  }
+
+  async run(message, reacoes) {
+    if (reacoes) return this.reactionConfig(message);
+
+    const embed = new MessageEmbed()
+      .setColor(this.client.settings.colors.LOUD_GREEN)
+      .setThumbnail(this.client.settings.images.LOUD_LOGO)
+      .setDescription(message.language.get('COMMAND_CONFIG_GENERAL_DESCRIPTION'))
+      .addField('Prefix', message.guild.settings.get('prefix'), true)
+      .addField(message.language.get('LANGUAGE'), message.language.name);
+
+    return message.send(embed);
+  }
+
+  async reactionConfig(message) {
+    const channels = await message.guild.settings.get('whitelistedChannels');
+    const keywords = await message.guild.settings.get('customReactions').keywords;
+    const responses = await message.guild.settings.get('customReactions').response;
+    const embed = new MessageEmbed()
+      .setColor(this.client.settings.colors.LOUD_GREEN)
+      .setThumbnail(this.client.settings.images.LOUD_LOGO)
+      .setDescription(message.language.get('COMMAND_CONFIG_REACTION_DESCRIPTION'))
+      .addField('Canais Permitidos', `<#${channels.join('>, <#')}>`)
+      .addField('Palavras-Chave', keywords.join(', '))
+      .addField('Respostas', `Existem **${responses.length}** respostas registradas.`);
+
+    return message.send(embed);
+  }
+};

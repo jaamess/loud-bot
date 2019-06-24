423a9bf28941cdcc31aa13dad1505eb83968f395
James <thiagozevedo3@gmail.com>
2019-06-24 04:04:55 -0300

    fix

--- events/customReaction.js
+++ events/customReaction.js
@@ -3,8 +3,11 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
   constructor(...args) {
     super(...args, {
+			name: 'customReaction',
+			event: 'customReaction',
       enabled: true,
-      once: false,
+			once: false,
+			event
     });
   }
 

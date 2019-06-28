0973c822cb2a82d7748f11381157d0d2dc830dbb
Nejc <ethoarx@gmail.com>
2019-06-28 08:36:07 +0200

    lets see

--- monitors/customReaction.js
+++ monitors/customReaction.js
@@ -1,29 +1,29 @@
 const { Monitor } = require('klasa');
 
 module.exports = class extends Monitor {
-  constructor(...args) {
-    super(...args, {
-      ignoreOthers: false,
-      ignoreEdits: false,
-    });
-  }
-  /**
-   * @param {import('klasa').KlasaMessage} message
-   */
-  async run(message) {
-    // Return if its a command
-    if (message.content.startsWith(message.guild.settings.prefix)) return;
-    // Getting whitelisted channels and keywords from the database
-    const whiteListedChannels = await message.guild.settings.get('whitelistedChannels');
-    const keywords = await message.guild.settings.get('customReactions').keywords;
-    const keywordInUserMessageContent = message.content.split(' ');
-    // If it's not a whitelisted channel or if it's not a keyword, do nothing
-    if (!whiteListedChannels.includes(message.channel.id)) return;
-    if (!keywordInUserMessageContent.some((k) => keywords.includes(k))) return;
 
-    const keyword = keywordInUserMessageContent.filter((key) => keywords.includes(key))[0];
+	constructor(...args) {
+		super(...args, {
+			ignoreOthers: false,
+			ignoreEdits: false
+		});
+	}
+	async run(message) {
+		// Return if its a command
+		if (!message.guild) return;
+		if (message.content.startsWith(message.guild.settings.get('prefix'))) return;
+		// Getting whitelisted channels and keywords from the database
+		const whiteListedChannels = await message.guild.settings.get('whitelistedChannels');
+		const keywords = await message.guild.settings.get('customReactions').keywords;
+		const keywordInUserMessageContent = message.content.split(' ');
+		// If it's not a whitelisted channel or if it's not a keyword, do nothing
+		if (!whiteListedChannels.includes(message.channel.id)) return;
+		if (!keywordInUserMessageContent.some((cont) => keywords.includes(cont))) return;
 
-    // Emit the event that will handle the custom reaction
-    return this.client.emit('customReaction', message, keyword);
-  }
+		const keyword = keywordInUserMessageContent.filter((key) => keywords.includes(key))[0];
+
+		// Emit the event that will handle the custom reaction
+		this.client.emit('customReaction', message, keyword);
+	}
+
 };

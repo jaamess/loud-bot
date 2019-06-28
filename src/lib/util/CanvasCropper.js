9f6a6209aee5c9c4edc09c0f4bc9e3e0de59e025
Nejc <ethoarx@gmail.com>
2019-06-28 08:06:41 +0200

    yay cropping

--- src/lib/util/CanvasCropper.js
+++ src/lib/util/CanvasCropper.js
@@ -1,9 +1,28 @@
+const fetch = require('chainfetch');
+const sizeOf = require('image-size');
+const { Canvas } = require('canvas-constructor');
+
 class CanvasCropper {
 
-	static crop() {
-		// here ye go
+	static async crop(image) {
+		image = await CanvasCropper._parseImage(image);
+		const dimensions = sizeOf(image);
+
+		const result = new Canvas(dimensions.width / 4, dimensions.height / 4)
+			.addImage(image, -((dimensions.width / 8) * 5), -((dimensions.height / 16) * 7), dimensions.width, dimensions.height);
+
+		return result.toBuffer();
+	}
+
+	static async _parseImage(image) {
+		if (CanvasCropper.isURL(image)) return fetch.get(image).toBuffer().onlyBody();
+		if (Buffer.isBuffer(image)) return image;
+		return new Buffer();
 	}
 
 }
+
+// eslint-disable-next-line
+CanvasCropper.URL_REGEX = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/;
 
 module.exports = CanvasCropper;

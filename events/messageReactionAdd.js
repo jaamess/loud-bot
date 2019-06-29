1e2640b5128f93e5964d47dba1efe7d3f37ce0fd
James <thiagozevedo3@gmail.com>
2019-06-28 22:57:13 -0300

    console logs

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,30 +1,31 @@
 const { Event } = require('klasa');
 
 module.exports = class extends Event {
+  constructor(...args) {
+    super(...args, {
+      name: 'messageReactionAdd',
+      enabled: true,
+      event: 'messageReactionAdd',
+      once: false,
+    });
+  }
+  async run(reaction, user) {
+    if (reaction.message.channel.parent.id === '589243529075752970') {
+      if (reaction.emoji.name === '❌') {
+        for (const users of reaction.users) {
+          // Uncomment this to allow moderators to react with "x"
+          // if (users.roles.position >= 27) return;
+          reaction.users.remove(user.id);
+        }
+      }
+    }
 
-	constructor(...args) {
-		super(...args, {
-			name: 'messageReactionAdd',
-			enabled: true,
-			event: 'messageReactionAdd',
-			once: false
-		});
-	}
-	async run(reaction, user) {
-		if (reaction.message.channel.parent.id === '589243529075752970') {
-			if (reaction.emoji.name === '❌') {
-				for (const users of reaction.users) {
-					// Uncomment this to allow moderators to react with "x"
-					// if (users.roles.position >= 27) return;
-					reaction.users.remove(user.id);
-				}
-			}
-		}
-
-		if (reaction.message.channel.id === '593493916293136424') {
-			const member = await reaction.message.guild.members.fetch(user).catch(() => console.error);
-			member.roles.add('593494969478676483');
-		}
-	}
-
+    if (reaction.message.channel.id === '593493916293136424') {
+      console.log('REACTION DETECTED');
+      const member = await reaction.message.guild.members.fetch(user).catch(() => console.error);
+      console.log('TRYING TO GIVE ROLE...');
+      member.roles.add('593494969478676483');
+      console.log('ROLE GIVEN!');
+    }
+  }
 };

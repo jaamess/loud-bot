a3ff575b299cd3775e25d28ebdb02a3e900af408
Nejc <ethoarx@gmail.com>
2019-06-29 07:11:18 +0200

    Comments

--- src/lib/util/CanvasCropper.js
+++ src/lib/util/CanvasCropper.js
@@ -5,11 +5,20 @@ const { Canvas } = require('canvas-constructor');
 class CanvasCropper {
 
 	static async crop(image) {
+		// Well just turn a URL, path or buffer into a valid buffer for Canvas
 		image = await Util.parseImage(image);
+		// Get the images size in width and height so that we can calculate relative positions later
 		const dimensions = sizeOf(image);
 
-		const result = new Canvas(dimensions.width / 4, dimensions.height / 4)
-			.addImage(image, -((dimensions.width / 8) * 5), -((dimensions.height / 16) * 7), dimensions.width, dimensions.height);
+		const cWidth = dimensions.width / 4;
+		const cHeight = dimensions.height / 4;
+		const iWidthOffset = -((dimensions.width / 8) * 5);
+		const iHeightOffset = -((dimensions.height / 16) * 7);
+
+		// Create a new canvas which has a 4th of the width and height of the original image
+		const result = new Canvas(cWidth, cHeight)
+			// Insert the image at a offset using the relative devidites of the width and height. Don't ask me how I got these. It was alot of trial and error.
+			.addImage(image, iWidthOffset, iHeightOffset, dimensions.width, dimensions.height);
 
 		return result.toBuffer();
 	}

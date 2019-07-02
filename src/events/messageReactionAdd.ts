69677b5cc2f80be06733155ce4ff72f916a6c3e9
James <thiagozevedo3@gmail.com>
2019-07-02 04:05:06 -0300

    messageReactionAdd to typescript

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -1,21 +1,20 @@
-const { Event } = require('klasa');
+import { Event, EventStore, KlasaUser } from 'klasa';
+import { MessageReaction, GuildMember } from 'discord.js';
 
-module.exports = class extends Event {
-  constructor(...args) {
-    super(...args, {
+export default class extends Event {
+  constructor(store: EventStore, file: string[], directory: string) {
+    super(store, file, directory, {
       name: 'messageReactionAdd',
       enabled: true,
       event: 'messageReactionAdd',
       once: false,
     });
   }
-  /**
-   *
-   * @param {import('discord.js').MessageReaction} reaction
-   */
-  async run(reaction, user) {
+
+  async run(reaction: MessageReaction, user: KlasaUser) {
     const whitelistedChannels = ['589243529075752970', '593493916293136424'];
     if (!whitelistedChannels.includes(reaction.message.channel.id)) return;
+    //@ts-ignore
     if (reaction.message.channel.parent.id === '589243529075752970') {
       if (reaction.emoji.name === '❌') {
         for (const users of reaction.users) {
@@ -27,7 +26,7 @@ module.exports = class extends Event {
     }
 
     if (reaction.message.channel.id === '593493916293136424') {
-      const member = await reaction.message.guild.members.fetch(user).catch(() => console.error);
+      const member = await reaction.message.guild.members.fetch(user).catch(() => console.error) as GuildMember;
       member.roles.add('593494969478676483');
       console.log(`Verified user in FF server successfully.`);
     }

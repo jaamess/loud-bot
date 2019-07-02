61d4f2378cbc6b99e261253fe1af24f77528de40
James <thiagozevedo3@gmail.com>
2019-07-02 17:46:57 -0300

    customReaction to typescript

--- src/events/customReaction.ts
+++ src/events/customReaction.ts
@@ -1,10 +1,10 @@
-const { Event } = require('klasa');
-const { MessageEmbed } = require('discord.js');
+import { Event, EventStore, KlasaMessage } from 'klasa';
+import { MessageEmbed } from 'discord.js';
 
-module.exports = class extends Event {
+export default class extends Event {
 
-	constructor(...args) {
-		super(...args, {
+	constructor(store: EventStore, file: string[], directory: string) {
+		super(store, file, directory, {
 			name: 'customReaction',
 			event: 'customReaction',
 			enabled: true,
@@ -12,17 +12,20 @@ module.exports = class extends Event {
 		});
 	}
 
-	async run(message, keyword) {
+	async run(message: KlasaMessage, keyword: string) {
 		// Getting the keywords and responses from the database:
 		// Since the keywords and responses are only added together,
 		// their indexes will always match, so we will be using that
 		// to match the keywords to their responses
+		//@ts-ignore
 		const keywords = await message.guild.settings.get('customReactions').keywords;
 		const index = keywords.indexOf(keyword);
+		//@ts-ignore
 		const response = await message.guild.settings.get('customReactions').response[index];
 		// If, by any means, we can't get the response, we will just do nothing
 		if (!response) return null;
 		const reactionEmbed = new MessageEmbed()
+			//@ts-ignore
 			.setColor(this.client.settings.colors.LOUD_GREEN)
 			.setTitle(`LOUD`)
 			.setDescription(response);

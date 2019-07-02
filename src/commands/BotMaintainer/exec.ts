f4d755fa694dfdfc7d95c88559f1e402f02858f9
James <thiagozevedo3@gmail.com>
2019-07-02 03:01:29 -0300

    exec to typescript

---
+++ src/commands/BotMaintainer/exec.ts
@@ -0,0 +1,21 @@
+import { Command, CommandStore, KlasaMessage, util } from 'klasa';
+
+export default class extends Command {
+  constructor(store: CommandStore, file: string[], directory: string) {
+    super(store, file, directory, {
+      aliases: ['execute'],
+      permissionLevel: 10,
+      guarded: true,
+      description: 'Executes commands in the terminal',
+      usage: '<expression:string>',
+    })
+  }
+
+  async run(message: KlasaMessage, input: any) {
+    const result = await util.exec(input, { timeout: `timeout` in message.flags ? Number(message.flags.timeout) : 60000 }).catch((error) => ({ stdout: null, stderr: error }));
+    const output = result.stdout ? `**\`OUTPUT\`**${util.codeBlock('prolog', result.stdout)}` : '';
+    const outerr = result.stderr ? `**\`ERROR\`**${util.codeBlock('prolog', result.stderr)}` : '';
+
+    return message.sendMessage([output, outerr].join('\n'));
+  }
+}

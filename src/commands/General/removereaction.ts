6a865c9661d63c814fa25f9961b022e324ab902b
James <thiagozevedo3@gmail.com>
2019-07-02 03:28:59 -0300

    delete unused file and migrate removereaction

--- src/commands/General/removereaction.ts
+++ src/commands/General/removereaction.ts
@@ -1,8 +1,8 @@
-const { Command } = require('klasa');
+import { Command, CommandStore, KlasaMessage } from 'klasa';
 
-module.exports = class extends Command {
-  constructor(...args) {
-    super(...args, {
+export default class extends Command {
+  constructor(store: CommandStore, file: string[], directory: string) {
+    super(store, file, directory, {
       runIn: ['text'],
       aliases: ['rm'],
       permissionLevel: 6,
@@ -12,15 +12,17 @@ module.exports = class extends Command {
     });
   }
 
-  async run(message, [keyword]) {
+  async run(message: KlasaMessage, [keyword]: [string[]]) {
     const customReactions = await message.guild.settings.get('customReactions');
+    //@ts-ignore
     if (!customReactions.keywords.includes(keyword))
       return message.send(
         `<:loudwarning:591525783994892288>  |  A palavra-chave "${keyword}" não existe no banco de dados. Diga \`loud config reacoes\` para visualizar as palavras-chave registradas no momento.`
       );
-
+    //@ts-ignore
     const index = await customReactions.keywords.indexOf(keyword);
     // Now we can delete those items from the database using update()
+    //@ts-ignore
     await message.guild.settings.update([['customReactions.keywords', keyword], ['customReactions.response', customReactions.response[index]]]);
     return message.send(`:white_check_mark:  **|  Feito, a palavra-chave ${keyword} e sua mensagem não existem mais.**`);
   }

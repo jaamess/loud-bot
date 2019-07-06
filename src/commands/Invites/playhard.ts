0b4ae423c307bd8b0646dc8ebe40666a3c284ea4
James <thiagozevedo3@gmail.com>
2019-07-06 15:48:09 -0300

    invite code commands

---
+++ src/commands/Invites/playhard.ts
@@ -0,0 +1,16 @@
+import { Command, CommandStore, KlasaMessage } from 'klasa';
+
+export default class extends Command {
+	public constructor(store: CommandStore, file: string[], directory: string) {
+    super(store, file, directory, {
+      runIn: ['text'],
+      aliases: ['ph'],
+      permissionLevel: 6,
+      description: 'Codigo de convite do Bruno Playhard'
+    });
+	}
+
+	async run(message: KlasaMessage): Promise<KlasaMessage | KlasaMessage[]> {
+		return message.send('https://discord.gg/playhard');
+	}
+}

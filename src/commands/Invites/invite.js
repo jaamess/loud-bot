f89ffd62fd63d3314b065b06a80a7f80b8e17480
James <thiagozevedo3@gmail.com>
2019-07-20 23:44:49 -0300

    invite command

---
+++ src/commands/Invites/invite.js
@@ -0,0 +1,36 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['inv'],
+			autoAliases: true,
+			bucket: 1,
+			cooldown: 10,
+			permissionLevel: 5,
+			description: 'Envia códigos de convite dos servidores verificados dos membros da LOUD.',
+			usage: '<server:string>'
+		});
+	}
+
+	async run(message, server) {
+		switch (server[0]) {
+			case 'loud': return this._loudInvite(message);
+			case 'bruno': return this._brunoInvite(message);
+			case 'crusher': return this._crusherInvite(message);
+		}
+		return message;
+	}
+
+	_loudInvite(message) {
+		return message.send('Convite do servidor da LOUD:\ndiscord.gg/loud');
+	}
+	_brunoInvite(message) {
+		return message.send('Convite do servidor do Bruno PH:\ndiscord.gg/playhard');
+	}
+	_crusherInvite(message) {
+		return message.send('Convite do servidor do Crusher:\ndiscord.gg/loudcrusher');
+	}
+
+};

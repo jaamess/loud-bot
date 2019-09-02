22276ebb965b19b538dfaaac847dd9fd3cf5cf6d
James <39378514+jaamess@users.noreply.github.com>
2019-09-02 12:33:29 -0300

    Update loudSignups.js

--- src/monitors/loudSignups.js
+++ src/monitors/loudSignups.js
@@ -11,12 +11,12 @@ module.exports = class extends Monitor {
 	}
 
 	async run(message) {
-		if (!message.channel.id === '595832094471749664') return;
+		/*if (!message.channel.id === '595832094471749664') return;
 		const screenshot = message.attachments.map((msg) => msg.attachment);
 		if (!screenshot.length) {
 			message.send('**Você deve enviar um vídeo de pelo menos 30 segundos e no máximo 1 minuto para se inscrever! Tente novamente.**');
 			return;
-		}
+		}*/
 	}
 
 };

7822569ef5645beff357755241e2db80170aeb68
James <thiagozevedo3@gmail.com>
2019-09-08 12:28:22 -0300

    fix

--- src/commands/Fun/lb.js
+++ src/commands/Fun/lb.js
@@ -13,7 +13,7 @@ module.exports = class extends Command {
 		// Fetch everyone
 		await message.guild.members.fetch()
 
-		const leaderboard = message.guild.members.sort((a, b) => b.user.settings.get('reputationPoints') - a.user.settings.get('reputationPoints')).map((member, index) => `${index + 1}. ${member.user.tag} :: ${member.user.settings.get('reputationPoints')}`).slice(0, 10).join('\n');
+		const leaderboard = message.guild.members.sort((a, b) => b.user.settings.get('reputationPoints') - a.user.settings.get('reputationPoints')).map((member, index) => `**${member.displayName}** :: *${member.user.settings.get('reputationPoints')}* pontos`).slice(0, 10);
 		console.log(leaderboard);
 		message.send('**Resultado enviado no console.**');
 	}

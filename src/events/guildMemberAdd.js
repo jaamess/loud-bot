e394bf8a9dc4245110a494b56c7e79973de5e75a
James <thiagozevedo3@gmail.com>
2019-09-30 03:06:39 -0300

    different channel

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -13,7 +13,7 @@ module.exports = class extends Event {
 		const brunoBot = ['brunoph', 'brunhophh', 'bruno playhard', 'bruno ph'];
 		if (brunoBot.includes(member.user.username.toLowerCase())) {
 			await member.ban({days: 7, reason: 'Self-bot do PH detectado'});
-			member.guild.channels.get('579470954166747146').send(`<:loudwarning:591525783994892288> * |  Self bot do PH detectado e banido. Usuario: <@${member.id}>. ID: ${member.id}*`);
+			member.guild.channels.get('628110340579917856').send(`<:loudwarning:591525783994892288> * |  Self bot do PH detectado e banido. Usuario: <@${member.id}>. ID: ${member.id}*`);
 			console.log(`Self-bot banido: ${member.user.username}#${member.user.discriminator}`);
 		}
 	}

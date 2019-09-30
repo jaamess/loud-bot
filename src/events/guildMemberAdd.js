d1fda4f28b073b4393ba49e334badbc64db70d2c
James <thiagozevedo3@gmail.com>
2019-09-30 02:18:51 -0300

    fix?

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -12,8 +12,9 @@ module.exports = class extends Event {
 
 		if (brunoBot.includes(message.member.username)) {
 			console.log(`Self-bot banido: ${member.user.username}#${member.user.discriminator}`);
+			await member.ban({days: 7, reason: 'Self-bot do PH detectado'});
+
 			member.guild.channels.get('579470954166747146').send(`:loudwarning: * |  Self bot do PH detectado e banido. Usuario: <@${member.id}>. ID: ${member.id}`);
-			member.ban({days: 7, reason: 'Self-bot do PH detectado'});
 		}
 	}
 

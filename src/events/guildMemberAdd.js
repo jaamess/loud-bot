9afce3a89384d05579cf11543f9eee2a0c36a58a
James <thiagozevedo3@gmail.com>
2019-09-30 02:20:10 -0300

    fix??

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -2,6 +2,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
 	constructor(...args) {
 		super(...args, {
+			name: 'guildMemberAdd',
 			enabled: true,
 			once: false
 		});

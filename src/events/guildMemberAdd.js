ee9d2b56cf9d8cbab7930fefd95e45e39222c99b
James <thiagozevedo3@gmail.com>
2019-10-03 15:04:27 -0300

    more brunoph name variations

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -13,11 +13,11 @@ module.exports = class extends Event {
 		console.log(member.user.username.toLowerCase());
 		const username = member.user.username.toLowerCase().trim();
 		console.log(username);
-		let brunoBot = ['bruno ph', 'brunoph', 'brunhophh', 'bruno playhard', 'bruno  ph'];
+		let brunoBot = ['bruno ph', 'brunoph', 'brunhophh', 'bruno playhard', 'bruno  ph', 'xbrunoph', 'bruno_ph', 'brunophx'];
 		console.log(brunoBot.includes(username));
 		if (brunoBot.includes(username)) {
 			await member.ban({days: 7, reason: 'Self-bot do PH detectado'});
-			member.guild.channels.get('628110340579917856').send(`<:loudwarning:591525783994892288>  *|  Self bot do PH detectado e banido. Usuario: <@${member.id}>. ID: ${member.id}*`);
+			member.guild.channels.get('628110340579917856').send(`<:loudwarning:591525783994892288>  |  *Self bot do PH detectado e banido. Usuario: <@${member.id}>. ID: ${member.id}*`);
 			console.log(`Self-bot banido: ${member.user.tag}`);
 			}
 	}

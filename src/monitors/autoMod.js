41a1cf0e817aa30c95a65a650727b489836cee60
Thiago Azevedo <thiagozevedo3@gmail.com>
2020-02-27 16:08:44 -0300

    adds automod feature

---
+++ src/monitors/autoMod.js
@@ -0,0 +1,36 @@
+const { Monitor } = require('klasa');
+module.exports = class extends Monitor {
+
+    constructor(...args) {
+         
+        super(...args, {
+            enabled: true,
+            ignoreBots: true,
+            ignoreSelf: true,
+            ignoreOthers: false,
+            ignoreWebhooks: true,
+            ignoreEdits: false
+        });
+    }
+
+    async run(message) {
+        if(message.channel.id !== '550196743099383818') return;
+        if(message.member.permissions.has('ADMINISTRATOR')) return;
+        // Forbidden words arrays
+        const forbiddenWords = 'guilda';
+        const politicaFutebol = ['vasco', 'bolsonaro', 'lula', 'brasileirão', 'futebol', 'bolsominion', 'petista', 'luladrão', 'salnorabo'];
+        // Words from the message
+        const words = message.content.split(' ').slice(0);
+
+        if(words.includes('guilda')) {
+            message.delete();
+            return message.reply(`<:loudwarning:591525783994892288>  |  *Para divulgar sua guilda, ou encontrar guildas que estejam recrutando, vá em: <#579117494598631424>.**`);
+        }
+
+        if(politicaFutebol.some(f => words.includes(f))) {
+            message.delete();
+            return message.reply(`<:loudwarning:591525783994892288>  |  *Discussões sobre política, futebol e religião não são permitidas! Esses assuntos causam brigas, então evitem comentar sobre.**`);
+        }
+    }
+
+};

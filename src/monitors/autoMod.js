98aa90032b33f3a77d3dc2a435d93d1579ed0afa
Thiago Azevedo <thiagozevedo3@gmail.com>
2020-02-27 16:14:57 -0300

    more autoMod

--- src/monitors/autoMod.js
+++ src/monitors/autoMod.js
@@ -19,17 +19,23 @@ module.exports = class extends Monitor {
         // Forbidden words arrays
         const forbiddenWords = 'guilda';
         const politicaFutebol = ['vasco', 'bolsonaro', 'lula', 'brasileirão', 'futebol', 'bolsominion', 'petista', 'luladrão', 'salnorabo'];
+        const vendas = ['vendo conta'];
         // Words from the message
         const words = message.content.split(' ').slice(0);
 
         if(words.includes('guilda')) {
             message.delete();
-            return message.reply(`<:loudwarning:591525783994892288>  |  *Para divulgar sua guilda, ou encontrar guildas que estejam recrutando, vá em: <#579117494598631424>.**`);
+            return message.reply(`<:loudwarning:591525783994892288>  |  **Para divulgar sua guilda, ou encontrar guildas que estejam recrutando, vá em: <#579117494598631424>.**`);
         }
 
         if(politicaFutebol.some(f => words.includes(f))) {
             message.delete();
-            return message.reply(`<:loudwarning:591525783994892288>  |  *Discussões sobre política, futebol e religião não são permitidas! Esses assuntos causam brigas, então evitem comentar sobre.**`);
+            return message.reply(`<:loudwarning:591525783994892288>  |  **Discussões sobre política, futebol e religião não são permitidas! Esses assuntos causam brigas, então evitem comentar sobre.**`);
+        }
+
+        if(vendas.some(v => words.includes(v))) {
+            message.delete();
+            return message.reply(`<:loudwarning:591525783994892288>  |  **Vendas não são permitidas no servidor, obrigado.**`);
         }
     }
 
